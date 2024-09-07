b715c962ca1058731ca69609ef71e2d6b9b0e48a
elzoz <elzoz@DESKTOP-OVU2248>
2024-09-08 01:38:51 +0300

    Vid 3

--- Session2/Program.cs
+++ Session2/Program.cs
@@ -6,8 +6,27 @@ namespace Session2
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-             
-            app.MapGet("/", () => "Hello from dev");
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Home", async context =>
+                {
+                    await context.Response.WriteAsync("You Are at Home Page");
+                });
+
+                endpoints.MapPost("/Product", async context =>
+                {
+                    await context.Response.WriteAsync("You Are at Product Page");
+                });
+            });
+
+            app.Run(async HttpContext =>
+            {
+                await HttpContext.Response.WriteAsync("Your Requested Page Not Found");
+
+            });
 
             app.Run();
         }
