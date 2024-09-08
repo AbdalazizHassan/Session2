49ef0d01708e9ce014f3b2b411088909de1da4b3
elzoz <elzoz@DESKTOP-OVU2248>
2024-09-08 04:40:47 +0300

    vid 5

--- Session2/Program.cs
+++ Session2/Program.cs
@@ -8,19 +8,7 @@ namespace Session2
             var app = builder.Build();
 
             app.UseRouting();
-
-            app.Use(async (context, next) =>
-            {
-                Endpoint endpoint = context.GetEndpoint();
-
-                if (endpoint == null)
-                    await context.Response.WriteAsync("Your Requested Page Not Found");
-
-                await next();
-            });
-
-
-
+         
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/Home", async context =>
@@ -28,10 +16,24 @@ namespace Session2
                     await context.Response.WriteAsync("You Are at Home Page");
                 });
 
-                endpoints.MapPost("/Product", async context =>
+                endpoints.MapGet("/Products", async context =>
                 {
-                    await context.Response.WriteAsync("You Are at Product Page");
+                    await context.Response.WriteAsync("You Are at Products Page");
                 });
+
+                endpoints.MapGet("/Products/{id}", async context =>
+                {
+                    int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                    await context.Response.WriteAsync($"Your Request Product With Id => {id}");
+                });
+
+                endpoints.MapGet("/Books/{id}/{author}", async context =>
+                {
+                    int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                    string Author = context.Request.RouteValues["Author"].ToString();
+                    await context.Response.WriteAsync($"Your Request Book With Id => {id} and Author => {Author}");
+                });
+
             });
 
             app.Run(async HttpContext =>
